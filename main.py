8bab41b05eed3ff3c0803933671a6311ccd233c6
Mathieu <matranc03@gmail.com>
2020-05-14 16:43:50 -0400

    cleanup

--- main.py
+++ main.py
@@ -20,21 +20,24 @@ def folder_creation():
     print("Preparing screenshots folder")
 
     if os.path.isdir("screenshots"):
-        print("Folder already exists")
+        print("Folder already exists\n")
     else:
         os.makedirs("screenshots")
-        print("Created screenshots folder")
+        print("Created screenshots folder\n")
 
 def move_files():
     for x in glob("Screen Shot*"):
         source = path+"/"+x
         destination = "screenshots/"
 
-        print(x)
+        print("Moving file: "+x)
         shutil.move(source, destination)
+        print("Filed moved successfully\n")
 
 confirmation()
 folder_creation()
 move_files()
+
+#Moved [number of files] in [time]
 
 print("done")
